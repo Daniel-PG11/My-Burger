ba3c990a62fbc43f78de5fe63ac1ad1eb40136d9
Daniel <daniel.p11198@gmail.com>
2021-03-11 08:34:34 -0800

    add responsive for side drawer

--- src/components/layout/layout.js
+++ src/components/layout/layout.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import Auxi from '../../hoc/auxi';
-import styles from './layout.module.css'
-import Toolbar from '../Navigation/Toolbar/Toolbar'
+import styles from './layout.module.css';
+import Toolbar from '../Navigation/Toolbar/Toolbar';
+import SideDrawer from '../Navigation/SideDrawer/SideDrawer';
 
 const layout = (props) =>
 (
     <Auxi>
         <Toolbar />
+        <SideDrawer/>
         <main className={styles.Content}>
             {props.children}
         </main>
