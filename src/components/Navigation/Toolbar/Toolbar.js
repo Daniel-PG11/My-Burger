fa9f662835a793a05dd5aae3822f0bcd8e4793d1
Daniel <daniel.p11198@gmail.com>
2021-03-12 22:53:53 -0800

    Add Toolbar and menu icon

--- src/components/Navigation/Toolbar/Toolbar.js
+++ src/components/Navigation/Toolbar/Toolbar.js
@@ -3,18 +3,17 @@ import React from 'react';
 import classes from './Toolbar.module.css';
 import Logo from '../../Logo/Logo';
 import NavigationItems from '../NavigationItems/NavigationItems';
-import navigationItems from '../NavigationItems/NavigationItems';
-
+import DrawerToggle from '../SideDrawer/DrawerToggle/DrawerToggle';
 const toolbar = (props) => (
-    <header className={classes.Toolbar}>
-        <div>MENU</div>
+         <header className={classes.Toolbar}>
+            <DrawerToggle clicked = {props.drawerToggleClicked}/>
             <div className={classes.Logo}>
                 <Logo />
             </div>
-        <nav className={classes.DesktopOnly}>
+            <nav className={classes.DesktopOnly}>
             <NavigationItems />
-        </nav>
-    </header>
+         </nav>
+        </header>
 );
 
 export default toolbar;
